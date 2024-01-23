c51c57941ea43b1909d95f988b71c186f4a861ac
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-01-23 17:22:09 +0000

    Practice - loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,10 +2,34 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
-# Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
+# Infinite Tacos -> Finite Tacos (control C to stop infinite loop)
+#loop do
+# puts "tacos!"
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# Instead of printing out each element via puts tacos [0], ...[1],... we need to first find out number of items in array
+# Puts "Eat #{tacos[0]}"
+
+# Set index variable
+index = 0
+
+#Start loop
+loop do
+
+# Break the loop if number of elements is same as index
+   if tacos.length == index
+    break 
+   end
+
+# Assign a variable to each taco
+taco = tacos[index]
+
+# Print the output
+puts "Eat #{taco} tacos!"
+
+# Increment then index
+    index = index + 1
+end
